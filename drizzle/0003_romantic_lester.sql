9cf7bdb19d07f49e9c226efccdd149191aa70873
StastkaJan <czsantafa@gmail.com>
2026-07-02 22:14:11 +0200

    fix migration: add week column before adding PK constraint

--- drizzle/0003_romantic_lester.sql
+++ drizzle/0003_romantic_lester.sql
@@ -1,3 +1,4 @@
+ALTER TABLE "week_slots" ADD COLUMN "week" date NOT NULL DEFAULT CURRENT_DATE;--> statement-breakpoint
 ALTER TABLE "week_slots" DROP CONSTRAINT "week_slots_plan_id_day_of_week_meal_type_pk";--> statement-breakpoint
 ALTER TABLE "week_slots" ADD CONSTRAINT "week_slots_plan_id_week_day_of_week_meal_type_pk" PRIMARY KEY("plan_id","week","day_of_week","meal_type");--> statement-breakpoint
-ALTER TABLE "week_slots" ADD COLUMN "week" date NOT NULL;
+ALTER TABLE "week_slots" ALTER COLUMN "week" DROP DEFAULT;
